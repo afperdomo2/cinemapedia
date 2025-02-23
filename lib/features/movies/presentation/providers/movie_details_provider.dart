02664110cb219356cb13170d8f5d65cad6ce44c1
alvarado0925 <alvarado0925@gmail.com>
2025-02-23 16:01:07 -0500

    Implementa la carga de detalles de películas en MovieScreen y agrega el proveedor movieDetailsProvider para gestionar el estado de carga.

---
+++ lib/features/movies/presentation/providers/movie_details_provider.dart
@@ -0,0 +1,30 @@
+import 'package:cinemapedia/features/movies/domain/entities/movie.dart';
+import 'package:cinemapedia/features/movies/presentation/providers/movie_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieDetailsProvider = StateNotifierProvider<MovieDetailsMapNotifier, Map<String, Movie>>((ref) {
+  final fetchMovieDetails = ref.watch(movieRepositoryProvider).getMovieDetails;
+  return MovieDetailsMapNotifier(fetchMovieDetails: fetchMovieDetails);
+});
+
+typedef MovieCallback = Future<Movie> Function(String movieId);
+
+class MovieDetailsMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final MovieCallback fetchMovieDetails;
+  bool isLoading = false;
+
+  MovieDetailsMapNotifier({
+    required this.fetchMovieDetails,
+  }) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state.containsKey(movieId)) return;
+    isLoading = true;
+
+    /// NOTE: Borrar esta línea
+    await Future.delayed(const Duration(milliseconds: 1000));
+    final Movie movie = await fetchMovieDetails(movieId);
+    state = {...state, movieId: movie};
+    isLoading = false;
+  }
+}
--- lib/features/movies/presentation/screens/movie_screen.dart
+++ lib/features/movies/presentation/screens/movie_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cinemapedia/features/movies/domain/entities/movie.dart';
+import 'package:cinemapedia/features/movies/presentation/providers/movie_details_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie_screen';
 
   final String movieId;
@@ -8,10 +11,30 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen(this.movieId, {super.key});
 
   @override
+  ConsumerState<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(movieDetailsProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieDetailsProvider)[widget.movieId];
+    final bool isLoading = ref.watch(movieDetailsProvider.notifier).isLoading;
+
+    if (isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie - $movieId'),
+        title: Text('Movie - ${widget.movieId}'),
       ),
       body: const Placeholder(),
     );

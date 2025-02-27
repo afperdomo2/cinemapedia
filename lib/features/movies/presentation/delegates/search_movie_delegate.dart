3de27372e22e9cdd60809251535ebc0e2f41b876
alvarado0925 <alvarado0925@gmail.com>
2025-02-27 00:04:30 -0500

    Agrega animaciones al botón de limpiar en el delegate de búsqueda de películas.

--- lib/features/movies/presentation/delegates/search_movie_delegate.dart
+++ lib/features/movies/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,27 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/features/movies/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar películas';
 
+  /// Este método se encarga de construir el widget que se muestra en la parte derecha del appbar.
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return const [
-      Text('buildActions'),
+    return [
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: const Duration(milliseconds: 200),
+        child: IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear)),
+      ),
     ];
   }
 
+  /// Este método se encarga de construir el widget que se muestra en la parte izquierda del appbar.
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(onPressed: () => close(context, null), icon: const Icon(Icons.arrow_back));
   }
 
   @override

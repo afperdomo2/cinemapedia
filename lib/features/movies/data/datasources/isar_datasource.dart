670774786953fb939f5603418bf4608bae9044be
alvarado0925 <alvarado0925@gmail.com>
2025-03-11 22:59:22 -0500

    Feat: Implementa el datasource Isar para la gestión de almacenamiento local de películas y agrega soporte para el plugin Path Provider

--- lib/features/movies/data/datasources/isar_datasource.dart
+++ lib/features/movies/data/datasources/isar_datasource.dart
@@ -1,7 +1,23 @@
 import 'package:cinemapedia/features/movies/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/features/movies/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDataSource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openIsar();
+  }
+
+  Future<Isar> openIsar() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    return (Isar.instanceNames.isEmpty)
+        ? await Isar.open([MovieSchema], inspector: true, directory: dir.path)
+        : Future.value(Isar.getInstance());
+  }
+
   @override
   Future<List<Movie>> getFavoriteMovies({int page = 1}) {
     // TODO: implement getFavoriteMovies
